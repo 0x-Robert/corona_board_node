b5439e3d9454312b2c4eefa02548214b33d714cb
shypang <smartdragon417@gmail.com>
2023-02-11 22:32:58 +0900

    [ADD] controller, utils, index

--- coronaboard-api/index.js
+++ coronaboard-api/index.js
@@ -1,34 +1,43 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const {sequelize} = require('./database')
+const express = require('express');
+const bodyParser = require('body-parser');
+const { sequelize } = require('./database');
 
-async function launchServer(){
-    const app = express()
-    app.use(bodyParser.json())
-    
-    app.get('/', (req, res) => {
-        res.json({message: "Hello coronaboard"});
-    });
+const globalStatController = require('./controller/global-stat.controller');
+const keyValueController = require('./controller/key-value.controller');
 
+async function launchServer() {
+  const app = express();
 
-    try{
+  // Content-Type이 application/json인 HTTP요청의 body를 파싱할 수 있게 해줍니다.
+  app.use(bodyParser.json());
 
-        await sequelize.sync();
-        console.log('database is ready')
-    }catch(error){
-        console.log('unable to connect to the database')
-        console.log(error);
-        process.exit(1)
-    }
+  app.get('/', (req, res) => {
+    res.json({ message: 'Hello CoronaBoard!' });
+  });
 
-    const port = process.env.PORT || 8888;
-    app.listen(port, ()=>{
-    console.log(`"Server is running on port ${port}"`)
-});
+  app.get('/global-stats', globalStatController.getAll);
+  app.post('/global-stats', globalStatController.insertOrUpdate);
+  app.delete('/global-stats', globalStatController.remove);
+
+  app.get('/key-value/:key', keyValueController.get);
+  app.post('/key-value', keyValueController.insertOrUpdate);
+  app.delete('/key-value/:key', keyValueController.remove);
+
+  try {
+    await sequelize.sync();
+    // only for dev
+    // sequelize.sync({ force: true });
+    console.log('Database is ready!');
+  } catch (error) {
+    console.log('Unable to connect to the database:');
+    console.log(error);
+    process.exit(1);
+  }
+
+  const port = process.env.PORT || 8888;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+  });
 }
 
 launchServer();
-
-
-
-
